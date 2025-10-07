95fbbca2f5f37c5c716fffee2393096d49886113
Vu Minh Thanh <thanhthbm@gmail.com>
2025-10-07 21:07:02 +0700

    Revert "first commit"
    This reverts commit a41dfc759416c49c8a502c7789462e81a73a887c.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get } from '@nestjs/common'
-import { AppService } from './app.service'
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
@@ -7,11 +7,6 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    return this.appService.getHello()
-  }
-
-  @Get('login')
-  getLogin(): string {
-    return this.appService.getLogin()
+    return this.appService.getHello();
   }
 }
